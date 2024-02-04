a8fa3deacfee1071f3b4fe5add8566b0d2bab2db
Maina Hyelavala <mhyelavala@gmail.com>
2024-02-05 00:33:42 +0100

    Adding Herosection Placeholder Text

--- src/App.js
+++ src/App.js
@@ -5,6 +5,17 @@ function App() {
     <section id='hero_section'>
       <div className='overlay'>
         <Nav/>
+
+        <div className='hero_txt'>
+          <h2>John "Yosh" Jaszcz</h2>
+          <p>
+            7 time Grammy winning Recording/Mixing Engineer 
+            and Producer, who has worked with a wide variety 
+            of artists in the music industry from George Clinton, 
+            Bootsy Collins, Billy Ray Cyrus, Kirk Franklin, 
+            Israel & New Breed and many more.
+          </p>
+        </div>
       </div>
     </section>
   );
