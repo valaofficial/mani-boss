2e0e31dc0fe422ba560392306ec06f6f5817a994
Maina Hyelavala <mhyelavala@gmail.com>
2024-02-05 00:15:23 +0100

    First Push

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Nav from './components/Nav';
+function App() {
+  return (
+    <section id='hero_section'>
+      <div className='overlay'>
+        <Nav/>
+      </div>
+    </section>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+function Nav() {
+  return (
+    <div className="main_nav">
+        <div className="nav_brand">
+            <h3>Producer</h3>
+        </div>
+        <div className="nav_links_div">
+            <ul>
+                <li>HOME</li>
+                <li>PRODUCTS</li>
+                <li>TESTIMONIALS</li>
+                <li>GALLERY</li>
+                <li>CONTACT</li>
+            </ul>
+        </div>
+    </div>
+  );
+}
+
+export default Nav;
